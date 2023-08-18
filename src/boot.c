01289ff3a0e2c7b9a72dd9f1aeec38b831074675
Žiga Miklošič <ziga.miklosic@gmail.com>
2023-08-18 18:00:46 +0200

    - added callback definitions - added debug prints

--- src/boot.c
+++ src/boot.c
@@ -44,6 +44,165 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Connect Bootloader Message Reception Callback
+*
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_connect_msg_rcv_cb(void)
+{
+
+
+    BOOT_DBG_PRINT( "Connect Bootloader Message Reception Callback..." );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Connect Response Bootloader Message Reception Callback
+*
+* @param[in]    msg_status - Status of connect command
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_connect_rsp_msg_rcv_cb(const boot_msg_status_t msg_status)
+{
+
+
+
+    BOOT_DBG_PRINT( "Connect Response Bootloader Message Reception Callback. Status: 0x%02X", msg_status );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Prepare Bootloader Message Reception Callback
+*
+* @param[in]    fw_size     - Firmware size in bytes
+* @param[in]    fw_ver      - Firmware version
+* @param[in]    hw_ver      - Hardware version
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_prepare_msg_rcv_cb(const uint32_t fw_size, const uint32_t fw_ver, const uint32_t hw_ver)
+{
+
+
+    BOOT_DBG_PRINT( "Prepare Bootloader Message Reception Callback..." );
+    BOOT_DBG_PRINT( "    - fw_size: %d bytes", fw_size );
+    BOOT_DBG_PRINT( "    - fw_ver:  0x%08X", fw_ver );
+    BOOT_DBG_PRINT( "    - hw_ver:  0x%08X", hw_ver );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Prepare Response Bootloader Message Reception Callback
+*
+* @param[in]    msg_status - Status of prepare command
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_prepare_rsp_msg_rcv_cb(const boot_msg_status_t msg_status)
+{
+
+
+    BOOT_DBG_PRINT( "Prepare Response Bootloader Message Reception Callback. Status: 0x%02X", msg_status );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Flash Bootloader Message Reception Callback
+*
+* @param[in]    p_data  - Flash binary data
+* @param[in]    size    - Size of flash data in bytes
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_flash_msg_rcv_cb(const uint8_t * const p_data, const uint16_t size)
+{
+
+    // Unused param
+    (void) p_data;
+
+    BOOT_DBG_PRINT( "Flash Bootloader Message Reception Callback..." );
+    BOOT_DBG_PRINT( "    - size: %d bytes", size );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Flash Response Bootloader Message Reception Callback
+*
+* @param[in]    msg_status - Status of flash command
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_flash_rsp_msg_rcv_cb(const boot_msg_status_t msg_status)
+{
+
+
+    BOOT_DBG_PRINT( "Flash Response Bootloader Message Reception Callback. Status: 0x%02X", msg_status );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Exit Bootloader Message Reception Callback
+*
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_exit_msg_rcv_cb(void)
+{
+
+
+    BOOT_DBG_PRINT( "Exit Bootloader Message Reception Callback..." );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Exit Response Bootloader Message Reception Callback
+*
+* @param[in]    msg_status - Status of exit command
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_exit_rsp_msg_rcv_cb(const boot_msg_status_t msg_status)
+{
+
+
+    BOOT_DBG_PRINT( "Exit Response Bootloader Message Reception Callback. Status: 0x%02X", msg_status );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Info Bootloader Message Reception Callback
+*
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_info_msg_rcv_cb(void)
+{
+
+
+    BOOT_DBG_PRINT( "Info Bootloader Message Reception Callback..." );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*       Info Response Bootloader Message Reception Callback
+*
+* @param[in]    msg_status  - Bootloader version
+* @param[in]    status      - Status of info command
+* @return       void
+*/
+////////////////////////////////////////////////////////////////////////////////
+void boot_com_info_rsp_msg_rcv_cb(const uint32_t boot_ver, const boot_msg_status_t msg_status)
+{
+
+
+    BOOT_DBG_PRINT( "Info Response Bootloader Message Reception Callback" );
+    BOOT_DBG_PRINT( "    - boot_ver: 0x%08X", boot_ver );
+    BOOT_DBG_PRINT( "    - Status:   0x%02X", msg_status );
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
@@ -83,8 +242,8 @@ boot_status_t boot_hndl(void)
 
     /// TESTING::::
 
-    boot_com_send_connect();
-    boot_com_send_flash((uint8_t*) "Hello", 5U );
+    //boot_com_send_connect();
+    //boot_com_send_flash((uint8_t*) "Hello", 5U );
 
 
     return status;
