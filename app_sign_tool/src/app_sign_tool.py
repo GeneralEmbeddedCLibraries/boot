b55d9f8af6726a0a038da2a2f15571070a8963fc
Žiga Miklošič <ziga.miklosic@gmail.com>
2023-08-22 16:32:19 +0200

    - added arg parser

--- app_sign_tool/src/app_sign_tool.py
+++ app_sign_tool/src/app_sign_tool.py
@@ -13,7 +13,7 @@
 #################################################################################################
 ##  IMPORTS
 #################################################################################################
-
+import argparse
 
 #################################################################################################
 ##  DEFINITIONS
@@ -22,9 +22,42 @@
 # Script version
 MAIN_SCRIPT_VER     = "V0.1.0"
 
+# Tool description
+TOOL_DESCRIPTION = \
+"Firmware Application Signature Tool %s" % MAIN_SCRIPT_VER
+
 #################################################################################################
 ##  FUNCTIONS
 #################################################################################################
+
+
+# ===============================================================================
+# @brief:   Argument parser
+#
+# @return:       TODO:...
+# ===============================================================================
+def arg_parser():
+
+    # Arg parser
+    parser = argparse.ArgumentParser( 	description=TOOL_DESCRIPTION, 
+                                        epilog="Enjoy the program!")
+
+    # Add arguments
+    parser.add_argument("-f",   help="Input binary file",               metavar="bin_in",           type=str,   required=True )
+    parser.add_argument("-o",   help="Output signed binary file",       metavar="bin_out",          type=str,   required=True )
+
+    # Get args
+    args = parser.parse_args()
+
+    # Convert namespace to dict
+    args = vars(args)
+
+    # Get arguments
+    file_in     = args["f"]
+    file_out    = args["o"]
+
+    return file_in, file_out
+
 
 
 # ===============================================================================
@@ -33,7 +66,19 @@ MAIN_SCRIPT_VER     = "V0.1.0"
 # @return: void
 # ===============================================================================
 def main():
-    pass
+    
+    file_in, file_out = arg_parser()
+
+    print( "Inputed file: %s" % file_in )
+    print( "Outputed file: %s" % file_out )
+
+
+    print("")
+    print("====================================================================")
+    print("     %s" % TOOL_DESCRIPTION )
+    print("====================================================================")
+    print("Firmware image successfuly signed!\n")
+
 
 
 
