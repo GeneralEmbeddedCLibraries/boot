cdfa3f8aef03f5ee2251d11d805e76289adb79c5
Žiga Miklošič <ziga.miklosic@seltron.si>
2023-08-23 13:33:27 +0200

    - added CRC calculations into application header

--- app_sign_tool/src/app_sign_tool.py
+++ app_sign_tool/src/app_sign_tool.py
@@ -17,14 +17,7 @@ import argparse
 import shutil
 
 import os
-
-
-
-# TODO: 
-#       1. Application header start & size customization
-#       2. 
-#
-#
+import struct
 
 #################################################################################################
 ##  DEFINITIONS
@@ -37,10 +30,21 @@ MAIN_SCRIPT_VER     = "V0.1.0"
 TOOL_DESCRIPTION = \
 "Firmware Application Signature Tool %s" % MAIN_SCRIPT_VER
 
+# Expected application header version
+APP_HEADER_VER_EXPECTED         = 1
+
+# Application header addresses
+APP_HEADER_APP_SIZE_ADDR        = 0x08
+APP_HEADER_APP_CRC_ADDR         = 0x0C
+APP_HEADER_VER_ADDR             = 0xFE
+APP_HEADER_CRC_ADDR             = 0xFF
+
+# Application header size in bytes
+APP_HEADER_SIZE_BYTE            = 0x100
+
 #################################################################################################
 ##  FUNCTIONS
 #################################################################################################
-
 
 # ===============================================================================
 # @brief:   Argument parser
@@ -69,7 +73,51 @@ def arg_parser():
 
     return file_in, file_out
 
+# ===============================================================================
+# @brief  Calculate CRC-32
+#
+# @param[in]    data    - Inputed data
+# @return       crc32   - Calculate CRC-32
+# ===============================================================================
+def calc_crc32(data):
+    poly = 0x04C11DB7
+    seed = 0x10101010
+    crc32 = seed
 
+    for byte in data:
+        crc32 = (( crc32 ^ byte ) & 0xFFFFFFFF )
+
+        for n in range( 32 ):
+
+            if crc32 & 0x80000000:
+                crc32 = (( crc32 << 1 ) ^ poly )
+            else:
+                crc32 = ( crc32 << 1 );
+
+    return crc32 & 0xFFFFFFFF 
+
+# ===============================================================================
+# @brief  Calculate CRC-8
+#
+# @param[in]    data    - Inputed data
+# @return       crc8    - Calculate CRC8
+# ===============================================================================
+def calc_crc8(data):
+    poly = 0x07
+    seed = 0xB6
+    crc8 = seed
+
+    for byte in data:
+        crc8 = (( crc8 ^ byte ) & 0xFF )
+
+        for n in range( 8 ):
+
+            if crc8 & 0x80:
+                crc8 = (( crc8 << 1 ) ^ poly )
+            else:
+                crc8 = ( crc8 << 1 );
+
+    return crc8 & 0xFF
 
 # ===============================================================================
 # @brief  Binary file Class
@@ -92,11 +140,11 @@ class BinFile:
     def __init__(self, file, access=READ_ONLY):
 
         # Store file name
-        self.file = file
+        self.file_name = file
 
         try:
-            if os.path.isfile(self.file):
-                self.file = open( self.file, access )
+            if os.path.isfile(file):
+                self.file = open( file, access )
         except Exception as e:
             print(e)
 
@@ -123,6 +171,11 @@ class BinFile:
         data = self.file.read(size)
         return data
     
+    # ===============================================================================
+    # @brief  Get size of binary file
+    #
+    # @return       data    - Readed data
+    # ===============================================================================    
     def size(self):
         return len( self.read( 0, None ))
 
@@ -138,7 +191,6 @@ class BinFile:
         self.file.seek(offset)                    
 
 
-
 # ===============================================================================
 # @brief:  Main 
 #
@@ -146,15 +198,12 @@ class BinFile:
 # ===============================================================================
 def main():
     
+    # Get arguments
     file_path_in, file_path_out = arg_parser()
 
-    print( "Inputed file: %s" % file_path_in )
-    print( "Outputed file: %s" % file_path_out )
-
-    print( file_path_in.split(".") )
-
     if "bin" != file_path_in.split(".")[1] or "bin" != file_path_out.split(".")[1]:
-        print( "ERROR: Invalid file format" ) 
+        print( "ERROR: Invalid file format" )
+        raise RuntimeError 
     
     else:
         # Copy inputed binary file
@@ -163,46 +212,42 @@ def main():
         # Open outputed binary file
         out_file = BinFile( file_path_out, access=BinFile.READ_WRITE)
 
-        # Calculate binary CRC
-        print("Size of binary: %s bytes" % out_file.size())
+        # Is application header version supported
+        if APP_HEADER_VER_EXPECTED == out_file.read( APP_HEADER_VER_ADDR, 1 )[0]:
 
-        # Count application size
-        # TODO: 
+            # Count application size
+            app_size = out_file.size()
 
+            # Calculate application CRC
+            app_crc = calc_crc32( out_file.read( 0, None ))
 
+            # Write app lenght into application header
+            out_file.write( APP_HEADER_APP_SIZE_ADDR, struct.pack('i', int(app_size)) )
 
+            # Write app CRC into application header
+            out_file.write( APP_HEADER_APP_CRC_ADDR, struct.pack('i', int(app_crc)) )
 
+            # Calculate application header CRC
+            app_header_crc = calc_crc8( out_file.read( 0, APP_HEADER_SIZE_BYTE ))
 
+            # Write application header crc
+            out_file.write( APP_HEADER_CRC_ADDR, [app_header_crc] )
 
+            print("")
+            print("====================================================================")
+            print("     %s" % TOOL_DESCRIPTION )
+            print("====================================================================")
+            print("Firmware image successfuly signed!\n")
 
-
-
-
-
-
-
-
-
-        print("")
-        print("====================================================================")
-        print("     %s" % TOOL_DESCRIPTION )
-        print("====================================================================")
-        print("Firmware image successfuly signed!\n")
-
-
-
-
-
-
-
-
+        else:
+            print( "ERROR: Application header version not supported!" ) 
+            raise RuntimeError 
 
 #################################################################################################
 ##  MAIN ENTRY
 #################################################################################################   
 if __name__ == "__main__":
     main()
-
 
 #################################################################################################
 ##  END OF FILE
